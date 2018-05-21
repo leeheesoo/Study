cdc82d3ca6e0e2e20b1ec0d359869e5c69f809fc
leeheesoo <tkfkdgmltn33@naver.com>
2018-05-21 13:23:34 +0900

    chapter13
    chapter13 퀴즈 마크업

---
+++ javascript/chapter13/script.js
@@ -0,0 +1,7 @@
+//문제
+var quizList = [
+    new quiz('다음 중 최초의 상용 웹 브라우저는?', ['모자이크', '인터넷익스플로러', '구글 크롬', '넷스케이프 네비게이터'], '넷스케이프 네비게이터'),
+    new quiz('웹 문서에서 스타일을 작성하는 언어는?', ['HTML', 'jQuery', 'CSS', 'XML'], 'CSS'),
+    new quiz('명령어 기반의 인터페이스를 의미하는 용어는?', ['GUI', 'CLI', 'HUD', 'SI'], 'CLI'),
+    new quiz('CSS 속성 중 글자의 굵기를 변경하는 속성은?', ['font-size', 'font-style', 'font-weight', 'font-variant'], 'font-weight')
+];
